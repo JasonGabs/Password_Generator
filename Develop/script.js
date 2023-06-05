abf4c747a2a5d6a560927d00f073595ef4f94335
Jason <jasongabs@gmail.com>
2023-06-05 14:11:11 -0700

    Added uppercase, lowercase, numeric, and special characters into a variable each, and put the conditionals of the password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,41 @@
 // Assignment code here
-
+var passwordLen;
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "|", "]", "{", ":", "?", ">", "<", "[", "]", ";", "'", ",", ".", "/", "-", "="]
 
 function generatePassword() {
 
-  prompt("How many characters?");
-  prompt("Would you like uppercase characters? y/n");
-  prompt("Would you like lowercase characters? y/n");
-  prompt("Would you like numeric characters? y/n");
-  prompt("Would you like special characters? y/n");
+  passwordLen = window.prompt("How many characters? ");
+  if (passwordLen < 8 || passwordLen > 128) {
+    return;
+  }
+  var uppercaseInput = window.confirm("Would you like uppercase characters?");
+  var lowercaseInput = window.confirm("Would you like lowercase characters?");
+  var numericInput = window.confirm("Would you like numeric characters?");
+  var specialCharacterInput = window.confirm("Would you like special characters?");
   
-};
+  var validCharacters = [];
 
-// generatePassword();
-// password.Math.random();
-// console.log(password);
+  if (uppercaseInput) {
+    validCharacters.concat(uppercase);
+  }
+  if (lowercaseInput) {
+    validCharacters.concat(lowercase);
+  }
+  if (numericInput) {
+    validCharacters.concat(numeric);
+  }
+  if (specialCharacterInput) {
+    validCharacters.concat(specialCharacters);
+  }
+
+  for (var i = 0; i < passwordLen; i++) {
+    var randomCharacter = Math.floor(Math.random() * .length);
+    
+  }
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -28,3 +50,17 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
+
+
+
+
+// This project is about creating a password generator. The majority of the HTML has been done for you, all you have to do is fill in the actual generator.
+
+// When the generate button is clicked, show the user the a series of prompts asking them for critieria. You will want to ask the following questions (it's recommended you use a confirm dialog unless otherwise specified):
+// What length do they want? (It's recommended you use a prompt for this)
+// Do they want uppercase letters?
+// Do they want lowercase letters?
+// Do they want numeric characters?
+// Do they want special characters?
+// If the user inputs invalid data (must select at least one character type, length must be between 8 and 128), then repeat the popups until they select correct data.
+// Once the criteria are selected, then use a random number generator to generate the new password and place it in the given space in the HTML. There are a number of ways to do this, the instructional staff can help with hints on this if you need.
